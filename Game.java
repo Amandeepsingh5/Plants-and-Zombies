31262cba0f5f9215cd658d2e6b9459062abf2b10
ToluAjisola <43460252+ToluAjisola@users.noreply.github.com>
2018-10-16 17:24:27 -0400

    Add files via upload

--- Game.java
+++ Game.java
@@ -2,19 +2,21 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+@SuppressWarnings("unused")
 public class Game {
 
 	Game f;
 	Zombie z;
-	
+	private static int lifes = 5;
 	private static ArrayList<Zombie> enemy;
 	private static int sun= 0;
 	private static int sunFlower = 0;
 	private static int peaShooter = 0;
+	private static int turns = 5;
+	private static int counter = 0;
 
 	public Game() {
 		Game.enemy = new ArrayList<Zombie>();
-
 	}
 
 	public void createSunflower() {
@@ -37,12 +39,14 @@ public class Game {
 			System.out.println(sun + " sun points");
 			System.out.println(sunFlower + " sunflower(s)");
 			System.out.println(" ");
+
 		}else if(x.equals("drop peashooter")) {
 			if(sun >= 3) {
 				peaShooter += 1;
 				System.out.println(peaShooter + " pea Shooter");
 				System.out.println(" ");
 				sun -=3;
+
 			}else {
 				System.out.println("Not enough sunpoints");
 				System.out.println(" ");
@@ -61,6 +65,7 @@ public class Game {
 						System.out.println("Zombie dead");
 						System.out.println(" ");
 					}
+					//System.out.println(enemy.size()+" left");
 				}
 				if(peaShooter > enemy.size())  {
 					int y = enemy.size();
@@ -88,24 +93,36 @@ public class Game {
 
 		Game f = new Game();
 		f.createEnemy();
-		while(enemy.isEmpty() == false) {
+		while(enemy.isEmpty() == false && lifes != 0) {
 
 			System.out.println("Commands");
-			System.out.println("drop sunflower");
-			System.out.println("drop peashooter");
-			System.out.println("shoot");
+			System.out.println("------------------");
+			System.out.println("1. drop sunflower");
+			System.out.println("2. drop peashooter");
+			System.out.println("3. shoot");
 			System.out.println(" ");
+
 
 			Scanner br = new Scanner(System.in);
 
-			sun += 1;
+			counter++;
+			if (counter == turns) {
+				lifes -= 1;
+				counter = 0;
+				System.out.println(lifes + " lives left");
+			}
+			sun++;
 			String name ;
 			name = br.nextLine();
 			f.action(name);
 
 		}
 
-		System.out.println("All zombies dead");
+		if(enemy.isEmpty() == false && lifes == 0) {
+			System.out.println("You died");
+		}else {
+			System.out.println("All zombies dead");
+		}
 
 	}
 }
