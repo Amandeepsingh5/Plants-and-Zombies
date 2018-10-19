f4030aa3158f063974467c690961bf5dea3adb6e
zohamehdi <43473034+zohamehdi@users.noreply.github.com>
2018-10-19 15:04:08 -0400

    Add files via upload

---
+++ level.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class level {
+	
+	private int distance;
+	private String description;
+	
+	public level(String description) {
+		this.description = description;
+	}
+	
+	public int setDistance(int lawn_distance) {
+		return this.distance = this.distance;
+	}
+	
+	public int getDistnace() {
+		return distance;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+}
